d58581fabb4e0669b267236d5b310c5c0e55b29c
iagocarvalho07 <71970033+iagocarvalho07@users.noreply.github.com>
2024-01-17 13:57:28 -0300

    l

--- lib/src/pages/authentication/repository/auth_errors.dart
+++ lib/src/pages/authentication/repository/auth_errors.dart
@@ -1,9 +1,15 @@
 String AuthErrorString(String? code) {
   switch (code) {
-    case 'IMVALID_CREDENTIALS':
+    case 'INVALID_CREDENTIALS':
       return 'Email e/ou senha invalidos';
     case "Invalid session token":
       return 'Token Invalido';
+    case 'INVALID_FULLNAME':
+        return 'OCORREU UM ERRO AO CADASTRAR USUARIO: NOME INVALIDO';
+    case 'INVALID_PHONE':
+      return 'OCORREU UM ERRO AO CADASTRAR USUARIO: Celular INVALIDO';
+    case 'INVALID_CPF':
+      return 'OCORREU UM ERRO AO CADASTRAR USUARIO: CPF INVALIDO';
     default:
       return "Error Desconhecido";
   }
--- lib/src/pages/authentication/repository/auth_repository.dart
+++ lib/src/pages/authentication/repository/auth_repository.dart
@@ -7,20 +7,21 @@ import 'package:grocerstore/src/services/http_manager.dart';
 class AuthRepository {
   final HttpManager _httpManager = HttpManager();
 
-  Future<AuthResult> validateToken(String token) async {
-    final result = await _httpManager.restRequest(
-      url: EndPoints.validateToken,
-      method: HttpMethods.post,
-      headers: {
-        'X-Parse-Session-Token' : token
-      }
-    );
+  AuthResult handleUserOrError(Map<dynamic, dynamic> result) {
     if (result['result'] != null) {
       final user = UserModel.fromJson(result['result']);
       return AuthResult.success(user);
     } else {
       return AuthResult.error(AuthErrorString(result['error']));
     }
+  }
+
+  Future<AuthResult> validateToken(String token) async {
+    final result = await _httpManager.restRequest(
+        url: EndPoints.validateToken,
+        method: HttpMethods.post,
+        headers: {'X-Parse-Session-Token': token});
+    return handleUserOrError(result);
   }
 
   Future<AuthResult> signIn({
@@ -35,11 +36,12 @@ class AuthRepository {
         "password": password,
       },
     );
-    if (result['result'] != null) {
-      final user = UserModel.fromJson(result['result']);
-      return AuthResult.success(user);
-    } else {
-      return AuthResult.error(AuthErrorString(result['error']));
-    }
+    return handleUserOrError(result);
+  }
+
+  Future<AuthResult> signUp(UserModel user) async {
+    final result = await _httpManager
+        .restRequest(url: EndPoints.signUp, method: HttpMethods.post, body: user.toJson());
+    return handleUserOrError(result);
   }
 }
